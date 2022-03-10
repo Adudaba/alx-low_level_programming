06d70114a51b3204573ebffcf6803abaa10ff257
Adudaba <adugnadaba21@gmail.com>
2022-03-10 01:39:16 -0800

    6th task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,8 +13,6 @@ int main(void)
 	char z;
 	float w;
 
-
-	
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(z));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(x));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(y));
